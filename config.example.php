7cb622a095ab81911884db851bfcbc400dc24905
Rakshitha M <rr5977623@gmail.com>
2026-08-24 23:12:46 +0530

    Secure database configuration

---
+++ config.example.php
@@ -0,0 +1,24 @@
+<?php
+
+function connection() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "YOUR_DATABASE_PASSWORD";
+    $dbname = "sms_php";
+
+    try {
+        $conn = new PDO(
+            "mysql:host=$servername;dbname=$dbname",
+            $username,
+            $password
+        );
+
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $conn;
+
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+}
+?>
